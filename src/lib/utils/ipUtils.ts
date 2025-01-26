bcf43f6c3b10f1cb78f1926891a506639017c1cc
Ryan Guimarães <guimaraesryan26@gmail.com>
2025-01-26 17:03:45 -0300

    Add cloudflare ip header first
    In case the website is proxied through cloudflare, they provide the users' ip address through the CF-Connecting-IP  header

--- src/lib/utils/ipUtils.ts
+++ src/lib/utils/ipUtils.ts
@@ -2,8 +2,18 @@ import { headers } from "next/headers";
 
 class IpAddressUtils {
 
-    public static getIpAddress() {
-        return headers().get('x-forwarded-for') || '127.0.0.1';
+    public static getIpAddress(): string {
+        const cfConnectingIp = headers().get('CF-Connecting-IP');
+        if (cfConnectingIp) {
+            return cfConnectingIp;
+        }
+
+        const xForwardedFor = headers().get('x-forwarded-for');
+        if (xForwardedFor) {
+            return xForwardedFor;
+        }
+
+        return '127.0.0.1';
     }
 
 }
